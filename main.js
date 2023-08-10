2b8bf0402f0517090b3efa4fbb74fd9f4f99d04e
anngyn <nguyenan1632004@gmail.com>
2023-08-10 12:04:40 +0700

    bai tap JS

---
+++ main.js
@@ -0,0 +1,62 @@
+/*var a = prompt("Nhap chieu dai")
+var b = prompt("Nhap chieu rong")
+
+alert("Dien tich tam giac: " + a*b)
+
+//bt2
+var x = prompt("So n");
+
+if(x % 2 == 0){
+    alert("x la so chan")
+}else {
+    alert("x la so le")
+}
+
+//bt3
+for(var i = 1; i <= 10; i++){
+    alert(i);
+}
+
+//BT4
+var x = 10;
+while(x != 0){
+    alert(x);
+    x--;
+}
+
+do{
+    alert(x);
+    x--;
+}while(x >= 0);
+
+
+
+//Bt5
+var k = prompt("Nhap k");
+for (var i = 0 ; i <= k; i ++){
+    alert(i);
+}
+var y = 0;
+while(y != k){
+    alert(y);
+    y++
+}
+
+
+/*
+var k = prompt("Nhap k");
+var y = 0;
+do{
+    alert(y);
+    y++
+}while(y <= k )
+
+*/
+
+var x = prompt("Nhap x");
+var sum = 0;
+for(var i = 1; i <=x ; i++){
+     sum += i;
+}
+alert(sum);
+
